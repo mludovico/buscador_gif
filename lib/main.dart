9b4b84aa857e660a1d68f356ff23324446aa8f0f
Marcelo Ludovico <marcelo.paula@eptv.com.br>
2019-07-02 21:24:05 -0300

    App finalizado até aula 86

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:buscador_gif/ui/HomePage.dart';
 
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white
+    ),
+  ));
+}
